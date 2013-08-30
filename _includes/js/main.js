f73c3939dedccfa08b09836cae6f5a55af771bee
hhlu <howard@howardlu.com>
2013-08-30 20:26:07 +0000

    made navbar stick with new method

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,6 +1,26 @@
 var disqus_shortname = 'howardlu';
 
 $(function() {
+    var $nav = $('nav.navbar');
+    var $header = $('header.main');
+
+    $(window).bind('scroll resize', function() {
+        if (
+            $(this).scrollTop() >= $header.offset().top + $header.innerHeight()
+        ) {
+            $nav.css({
+                left: 0,
+                position: 'fixed',
+                top: 0
+            });
+        }
+        else {
+            $nav.css({
+                position: 'static'
+            });
+        }
+    });
+
     var blogSearch = new BlogSearch();
     blogSearch.bindControls();
     blogSearch.bindSearch();
