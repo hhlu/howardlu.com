f77ceaf3a77985222ecfe9dabc3ac4fdda6cf42f
hhlu <howard@howardlu.com>
2013-08-30 06:29:27 +0000

    navbar updates

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,11 +1,6 @@
 var disqus_shortname = 'howardlu';
 
 $(function() {
-    var stickyPanelOptions = {
-        savePanelSpace: true
-    };
-    $('nav.navbar').stickyPanel(stickyPanelOptions);
-
     var blogSearch = new BlogSearch();
     blogSearch.bindControls();
     blogSearch.bindSearch();
--- js/plugins.js
+++ js/plugins.js
@@ -3,4 +3,3 @@
 {% include js/vendor/modernizr.js %}
 {% include js/vendor/underscore.js %}
 {% include js/vendor/bootstrap.min.js %}
-{% include js/vendor/jquery.stickypanel.js %}
